50163862888f045a68969e1c8d77ce85fb07ef2d
fishxmeow <202003773@student.polytechnic.bh>
2024-05-25 05:41:51 -0700

    started on level 3
    meow

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,91 +1,133 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 10f;
-    public float dashForce = 20f;
-    public float dashDuration = 0.2f;
-    public float crouchScale = 0.5f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float dashDistance = 5f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float crouchScale = 0.5f;
     private Rigidbody rb;
+    private Vector2 movementInput;
     private bool isGrounded;
     private bool isDashing;
     private bool isCrouching;
-    private float originalScale;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true; // Freeze rotation along all axes
-        originalScale = transform.localScale.y;
+        isGrounded = true;
+        isDashing = false;
+        isCrouching = false;
     }
 
     private void Update()
     {
-        // Check if player is grounded
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
-
-        // Player movement
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
-        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
-
-        // Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (!isDashing)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // Apply movement in the Update method for smoother input response
+            Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
+            rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movement.z * speed);
         }
 
-        // Dash
-        if (Input.GetKeyDown(KeyCode.LeftControl) && !isDashing)
+        if (isGrounded && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            StartCoroutine(Dash());
+            Jump();
         }
 
-        // Crouch
-        if (Input.GetKeyDown(KeyCode.X))
+        if (!isDashing && Keyboard.current.leftShiftKey.wasPressedThisFrame)
         {
-            if (!isCrouching)
+            Dash();
+        }
+
+        if (!isDashing && Keyboard.current.leftCtrlKey.isPressed)
+        {
+            if (CrouchSettings.isToggleCrouchEnabled)
             {
-                Crouch();
+                ToggleCrouch();
             }
             else
             {
-                Uncrouch();
+                HoldCrouch();
             }
         }
+        else
+        {
+            StopCrouch();
+        }
     }
 
-    private void Crouch()
+    private void Jump()
     {
-        transform.localScale = new Vector3(transform.localScale.x, originalScale * crouchScale, transform.localScale.z);
-        isCrouching = true;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
     }
 
-    private void Uncrouch()
+    private void Dash()
     {
-        transform.localScale = new Vector3(transform.localScale.x, originalScale, transform.localScale.z);
-        isCrouching = false;
+        StartCoroutine(DashCoroutine());
     }
 
-    private IEnumerator Dash()
+    private IEnumerator DashCoroutine()
     {
         isDashing = true;
-        float dashTime = 0f;
-        Vector3 dashDirection = rb.velocity.normalized;
+        Vector3 dashDirection = new Vector3(movementInput.x, 0f, movementInput.y).normalized;
+        Vector3 dashVelocity = dashDirection * dashDistance / dashDuration;
 
-        while (dashTime < dashDuration)
-        {
-            rb.AddForce(dashDirection * dashForce, ForceMode.Impulse);
-            dashTime += Time.deltaTime;
-            yield return null;
-        }
+        rb.velocity = dashVelocity;
 
+        yield return new WaitForSeconds(dashDuration);
+
+        rb.velocity = Vector3.zero;
         isDashing = false;
+    }
+
+    private void ToggleCrouch()
+    {
+        if (!isCrouching)
+        {
+            isCrouching = true;
+            transform.localScale = new Vector3(1f, crouchScale, 1f);
+        }
+        else
+        {
+            isCrouching = false;
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    private void HoldCrouch()
+    {
+        if (!isCrouching)
+        {
+            isCrouching = true;
+            transform.localScale = new Vector3(1f, crouchScale, 1f);
+        }
+    }
+
+    private void StopCrouch()
+    {
+        if (isCrouching)
+        {
+            isCrouching = false;
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void OnMove(InputValue movementValue)
+    {
+        // Get movement input value from the Input System
+        movementInput = movementValue.Get<Vector2>();
     }
 }
